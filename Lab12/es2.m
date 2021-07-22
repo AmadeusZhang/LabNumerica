3211c1efc1c6b48f48386b960a1ebc36a6493579
zhzj <amadeuszhang@outlook.com>
2021-07-22 15:30:04 +0200

    implemented heum method

---
+++ Lab12/es2.m
@@ -0,0 +1,31 @@
+% Esercizio 2 Laboratorio 12
+
+clear; close all; clc
+
+%% a) implementazione funzione Heun
+
+f = @(t, y) y ./ 3*t + ( log(t) + 1 ) ./ 3*t;
+
+y0 = -2;
+
+t_0 = 1; t_max = 10;
+
+%% b) risoluzione problema di Cauchy
+
+% approssimazione col metodo di Heun
+h = 1/5;
+[ th, uh ] = heun( f, t_0, t_max, y0, h );
+
+% soluzione esatta
+xvect = linspace( t_0, t_max );
+
+f_ex = @(t) 2 * t.^(1/3) - log(t) - 4;
+yvect = f_ex( xvect );
+
+% plot:
+plot( xvect, yvect, 'k--', 'LineWidth', 1 );
+hold on; grid on;
+
+% plot( th, uh );
+% title('metodo di Heun');
+% legend('I_{ex}', 'I_{Heun}');
---
+++ Lab12/heun.m
@@ -0,0 +1,21 @@
+% Implementazione del metodo di Heun per risoluzione del problema di Cauchy
+
+% metodo che sfrutta l'approssimazione degli integrali
+
+function [ th, uh ] = heun( f, t_0, t_max, y0, h )
+% creare il vettore degli instanti temporali --> :
+th = t_0: h: t_max;
+
+% calcolare lunghezza del vettore temporale
+N = length( th );
+
+% inizializzare vettore soluzione
+uh = zeros( 1, N );
+uh(1) = y0;
+    
+% ciclo for:
+for n = 1: N-1
+    uh(n+1) = uh(n) + (h/2) * ( f(th(n), uh(n)) + f( th(n+1), uh(n)+h*f(th(n), uh(n)) ) );
+end
+
+end
