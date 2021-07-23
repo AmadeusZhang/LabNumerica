4efe77bc74dbbc02fb3bc57044ad68bc7258469d
zhzj <amadeuszhang@outlook.com>
2021-07-23 09:40:57 +0200

    added CN.m and finished es2.m

--- Lab12/es2.m
+++ Lab12/es2.m
@@ -1,10 +1,10 @@
 % Esercizio 2 Laboratorio 12
 
-clear; close all; clc
+clear; clc
 
 %% a) implementazione funzione Heun
 
-f = @(t, y) y ./ (3*t) + ( log(t) + 1 ) ./ 3*t;
+f = @(t, y) y ./ (3*t) + ( log(t) + 1 ) ./ (3*t);
 
 y0 = -2;
 
@@ -14,7 +14,7 @@ t_0 = 1; t_max = 10;
 
 % approssimazione col metodo di Heun
 h = 1/5;
-[ th, uh ] = heun( f, t_0, t_max, y0, h );
+[ th_heun, uh_heun ] = heun( f, t_0, t_max, y0, h );
 
 % soluzione esatta
 xvect = linspace( t_0, t_max );
@@ -26,6 +26,51 @@ yvect = f_ex( xvect );
 plot( xvect, yvect, 'k--', 'LineWidth', 1 );
 hold on; grid on;
 
-% plot( th, uh );
-% title('metodo di Heun');
-% legend('I_{ex}', 'I_{Heun}');
+plot( th_heun, uh_heun );
+title('metodo di Heun e CN');
+
+%% c) implementazione il metodo di Crank-Nicolson
+% approssimazione col metodo di CN
+h = 1/5;
+[ th_CN, uh_CN, iter ] = CN( f, t_0, t_max, y0, h );
+
+% plot:
+plot( th_CN, uh_CN );
+
+legend('I_{ex}', 'I_{Heun}', 'I_{CN}');
+
+%% andamento degli errori
+H = [ 1/5 1/10 1/20 1/40 ];
+N = length(H);
+
+% creare il vettore nullo
+err_heun = zeros( 1, N );
+err_CN = zeros( 1, N );
+
+% ciclo di iterazione:
+for ii = 1: N
+    % calcolo le approssimazioni
+    [ th_CN, uh_CN, iter ] = CN( f, t_0, t_max, y0, H(ii) );
+    [ th_heun, uh_heun ] = heun( f, t_0, t_max, y0, H(ii) );
+    
+    % plot:
+    plot( th_heun, uh_heun, 'b', th_heun, f_ex(th_heun), 'r--' );
+    pause(1)
+    
+    
+    % calcolo gli errori
+    err_heun(ii) = norm( f_ex(th_CN) - uh_CN, inf );
+    err_CN(ii) = norm( f_ex(th_heun) - uh_heun, inf );
+    
+end
+
+% plot
+figure();
+loglog( H, err_CN, H, err_heun, H, H, '--', H, H.^2, '--' );
+grid on;
+title('Confronto degli errori');
+legend('err_{CN}', 'err_{heun}', 'H', 'H^2');
+
+% commento:
+% dal grafico si evince che entambi CN e Heun hanno un andamento parallelo
+% ad H^2, pertanto ordine di convergenza 2.
